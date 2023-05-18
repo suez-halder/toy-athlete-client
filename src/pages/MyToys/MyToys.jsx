3b3d579ab4081f02dd7c170c3ea4e18d1c1c5f7a
suezhalder <szhldr@gmail.com>
2023-05-18 19:47:40 +0600

    myToys page created

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import ToyRow from "../AllToys/ToyRow";
+import ToyBanner from "../Shared/ToyBanner";
 
 
 const MyToys = () => {
+    const { user } = useContext(AuthContext);
+    const [myToys, setMyToys] = useState([])
+
+    const url = `http://localhost:3000/allToys?email=${user?.email}`
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setMyToys(data)
+            })
+    }, [url])
+
+    
+    
+    
     return (
         <div>
-            <h2>My Toys here</h2>
+            <div><ToyBanner>My Added Toys</ToyBanner></div>
+            <div className="overflow-x-auto mt-8 border border-blue-400">
+                    <table className="table table-zebra w-full text-center">
+                        {/* head */}
+                        <thead >
+                            <tr>
+                                <th>Seller Name</th>
+                                <th>Toy Name</th>
+                                <th>Sub-category</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                                {
+                                    myToys.map((toyRow) => <ToyRow
+                                    key={toyRow._id}
+                                    toyRow={toyRow}
+                                 
+                                    ></ToyRow>)
+                                }
+                            {/* row 2 */}
+                            
+                        </tbody>
+                    </table>
+                </div>
         </div>
     );
 };
--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -1,15 +1,21 @@
 import { useLoaderData } from "react-router-dom";
+import ToyBanner from "../Shared/ToyBanner";
 
 
 const ToyDetails = () => {
 
     const toyDetails = useLoaderData()
-    console.log(toyDetails);
+    // console.log(toyDetails);
+    const {description, email, name, price, quantity, rating, toyName, toyPhoto} = toyDetails;
     
     
     return (
         <div>
-            <h2>Single Toy Details here</h2>
+            <div><ToyBanner>{toyName} Details Here</ToyBanner></div>
+            <div>
+                <img src={toyPhoto} alt="" />
+            </div>
+
         </div>
     );
 };
