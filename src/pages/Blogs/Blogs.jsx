8673a215b6b92dde6e818038b3d986ba57befc2a
suezhalder <szhldr@gmail.com>
2023-05-19 18:07:22 +0600

    blogs done

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,9 +1,72 @@
+import ToyBanner from "../Shared/ToyBanner";
 
 
 const Blogs = () => {
     return (
         <div>
-            Blogs here
+            <ToyBanner>Frequently Asked Questions</ToyBanner>
+            <div className="my-4 md:my-8  w-full md:w-5/6 gap-4 mx-auto grid grid-cols-1 md:grid-cols-2">
+                <div tabIndex={0} className="collapse collapse-arrow border border-sky-300 bg-sky-100 rounded-box my-2 md:my-4">
+                    <div className="collapse-title text-xl font-medium ">
+                        What is an access token and refresh token? How do they work and where should we store them on the client-side?
+                    </div>
+                    <div className="collapse-content mt-4">
+                        <p>
+                            Access Token → Passport
+                            Refresh Token → NID
+                        </p>
+                        <br />
+                        <p>
+                            If the Passport becomes invalid, it can be repeatedly renewed using the NID. However, if the NID is lost or becomes invalid, it will be necessary to go through the entire verification process again to obtain a new NID. After that, the Passport can be created or renewed again.
+                        </p>
+                        <br />
+                        <p>   If the Access Token expires, it can be repeatedly accessed using the Refresh Token. However, if the Refresh Token expires, then it will be necessary to authenticate again from scratch.</p>
+                    </div>
+                </div>
+                <div tabIndex={1} className="collapse collapse-arrow border border-sky-300 bg-sky-100 rounded-box my-2 md:my-4">
+                    <div className="collapse-title text-xl font-medium">
+                        Compare SQL and NoSQL databases?
+                    </div>
+                    <div className="collapse-content mt-4">
+                        <p>
+                            SQL databases have a fixed structure with tables and columns to store data. They are good for organized and structured data but can be difficult to scale.
+                        </p>
+                        <br />
+                        <p>
+                            NoSQL databases are more flexible and can handle different types of data, making them easier to scale horizontally across multiple servers.
+                        </p>
+                    </div>
+                </div>
+                <div tabIndex={2} className="collapse collapse-arrow border border-sky-300 bg-sky-100 rounded-box my-2 md:my-4">
+                    <div className="collapse-title text-xl font-medium">
+                        What is express js? What is Nest JS?
+                    </div>
+                    <div className="collapse-content mt-4">
+                        <p>
+                            Express.js is a popular web application framework for Node.js. It simplifies the creation of web servers and handling HTTP requests. It is beginner-friendly and widely used.
+                        </p>
+                        <br />
+                        <p>
+                            Nest.js is a TypeScript-based framework that builds on top of Express.js, providing additional features like dependency injection and modular architecture for creating scalable and maintainable applications.
+                        </p>
+                    </div>
+                </div>
+                <div tabIndex={3} className="collapse collapse-arrow border border-sky-300 bg-sky-100 rounded-box my-2 md:my-4">
+                    <div className="collapse-title text-xl font-medium">
+                        What is MongoDB aggregate and how does it work?
+                    </div>
+                    <div className="collapse-content mt-4">
+                        <p>
+                            MongoDB Aggregate is a feature that allows you to perform complex data analysis and transformations on data stored in MongoDB.
+                        </p>
+                        <br />
+                        <p>
+                            It uses a pipeline-based approach, where you can chain multiple stages together to filter, group, project, sort, and perform computations on data. It helps in aggregating and summarizing data in a flexible manner.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -21,16 +21,16 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-blue-100 rounded-box w-28">
-                    <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/'>Home</Link></li>
-                    <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/blogs'>Blogs</Link></li>
-                    <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/allToys'>All  Toys</Link></li>
-                    {
-                        user &&
-                        <>
-                            <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/myToys'>My Toys</Link></li>
-                            <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/addToy'>Add Toy</Link></li>
-                        </>
-                    }
+                        <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/'>Home</Link></li>
+                        <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/blogs'>Blogs</Link></li>
+                        <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/allToys'>All  Toys</Link></li>
+                        {
+                            user &&
+                            <>
+                                <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/myToys'>My Toys</Link></li>
+                                <li className="hover:bg-info rounded-md md:tracking-wider"><Link to='/addToy'>Add Toy</Link></li>
+                            </>
+                        }
 
                     </ul>
                 </div>
@@ -53,8 +53,8 @@ const Header = () => {
             </div>
             <div className="navbar-end">
                 {
-                    
-                    user?.photoURL && <img className="rounded-full h-10 mr-2" src={user.photoURL} alt="" title={user.displayName}/>
+
+                    user?.photoURL && <img className="rounded-full h-10 mr-2" src={user.photoURL} alt="" title={user.displayName} />
                 }
                 {
                     user ? <Link to='login'><button onClick={logOut} className="btn bg-[#4CB7CF] hover:bg-info border-none px-8 md:px-12 md:tracking-widest">Log Out</button></Link> : <Link to='login'><button className="btn bg-[#4CB7CF] hover:bg-info border-none px-8 md:px-12 md:tracking-widest">Login</button></Link>
