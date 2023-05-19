a3df38f47a015d33ce7306c5246d94a6efd320c0
suezhalder <szhldr@gmail.com>
2023-05-20 01:10:54 +0600

    spinner added

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,6 +2,8 @@
 import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
+import Lottie from "lottie-react";
+import loadingAnimation from "../assets/loading.json";
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
@@ -9,8 +11,9 @@ const PrivateRoute = ({ children }) => {
 
     if (loading) {
         return (
-            <div className='text-center my-60'>
-                <progress className="progress w-56 "></progress>
+            <div className='text-center'>
+                {/* <progress className="progress w-56 "></progress> */}
+                <Lottie className='w-1/2 mx-auto' animationData={loadingAnimation} />;
             </div>
         )
 
