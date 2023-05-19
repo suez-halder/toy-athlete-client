ac6d2a953ab65696774d8735429674998fa63547
suezhalder <szhldr@gmail.com>
2023-05-19 19:46:03 +0600

    search functionality added

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,20 +1,49 @@
 import { useLoaderData } from "react-router-dom";
 import ToyBanner from "../Shared/ToyBanner";
-import ToyRow from "./ToyRow";
 
+import ToyRow from "./ToyRow";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 const AllToys = () => {
-
     const allToys = useLoaderData()
+    const [searchText, setSearchText] = useState("");
+    const [toySearch, setToySearch] = useState(allToys)
+
+
+    // console.log(toySearch);
     // console.log(allToys);
-    
-   
-    
+
+
+    const handleSearch = () => {
+
+        fetch(`http://localhost:3000/alltoys/${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+                setToySearch(data);
+            })
+            
+
+    }
+
     return (
         <div>
             <div><ToyBanner>All Toys List</ToyBanner></div>
+            <div className="w-1/5 mx-auto">
+                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7 flex items-center">
+                    <div className="relative">
+                        <input onChange={(e) => setSearchText(e.target.value)} id="search" name="search" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Search Toy" />
+                        <label htmlFor="search" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm text-center">Search by Toy Name</label>
+                    </div>
+                    <div className="text-center">
+                        <button onClick={() => handleSearch()} className="bg-[#4CB7CF] hover:bg-info text-white rounded-md p-3 mb-4"><FaSearch></FaSearch> </button>
+                    </div>
+                </div>
+            </div>
+            
+            {/* table */}
             <div>
-                <div className="overflow-x-auto mt-8 border border-blue-300 rounded-xl">
+                <div className="overflow-x-auto border border-blue-300 rounded-xl">
                     <table className="table table-zebra w-full text-center">
                         {/* head */}
                         <thead >
@@ -30,15 +59,15 @@ const AllToys = () => {
                         </thead>
                         <tbody>
                             {/* row 1 */}
-                                {
-                                    allToys.map((toyRow) => <ToyRow
+                            {
+                                toySearch.map((toyRow) => <ToyRow
                                     key={toyRow._id}
                                     toyRow={toyRow}
-                                 
-                                    ></ToyRow>)
-                                }
+
+                                ></ToyRow>)
+                            }
                             {/* row 2 */}
-                            
+
                         </tbody>
                     </table>
                 </div>
